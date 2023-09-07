7c240b7c2c286b63b219a89e6f3643536c2659d0
Aryav <aryavbhola@gmail.com>
2023-09-07 20:52:13 +0530

    Favicon Changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head />
+			<link rel="icon" href="/favicon_light.png" />
+			<link rel="icon" href="/favicon_dark.png" media="(prefers-color-scheme: dark)" />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
