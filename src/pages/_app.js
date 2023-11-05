0e221fe1d6c9d091105ef864501ec849fd4fc506
Aryav <aryavbhola@gmail.com>
2023-11-05 15:33:33 +0530

    Added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { Inter } from "next/font/google";
 import Sidebar from "../components/Sidebar";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }) {
       <Sidebar />
       <main className={`flex flex-col w-3/4 ml-[25%] p-8 gap-4 max-lg:w-screen max-lg:ml-0`}>
         <Component {...pageProps} />
+        <Analytics />
       </main>
 		</main>
   )
